ad2763054c4835298d04bd4b2417a7682ff490b6
mata0050 <mata0050@outlook.com>
2021-09-22 11:00:11 -0400

    conditional execution

---
+++ conditionals.js
@@ -0,0 +1,37 @@
+const raining = true;
+const cold = false;
+
+if(raining){
+  console.log("Don't forget your umbrella!");
+}
+
+if(cold){
+  console.log("Make sure you pick out a scart!");
+}
+
+console.log("Now your're ready to go outside");
+
+// checking the temperature
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+// logical operators
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
