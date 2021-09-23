c001b770df29fc44732519749df59f675c8c8e54
mata0050 <mata0050@outlook.com>
2021-09-23 18:36:32 -0400

    Use a while loop to repeat a chorus line

--- tinkering.js
+++ tinkering.js
@@ -1,6 +1,17 @@
-function multiply(a, b) {
-  return a * b;
+const whichSchool = function(age){
+  if(age >=1 && age < 13 ){
+    return "Elementary School"
+  }else if(age >= 13 && age <=18){
+    return "Secondary School"
+  }else if(age <= 100){
+   return "Lighthouse Labs"
+  }
 }
 
-const results = multiply(2, 4);
-console.log(results)
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(30));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(12));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(19));
